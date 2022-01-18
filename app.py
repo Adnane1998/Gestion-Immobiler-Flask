f8f86bf7fb9068e55eb13ef3757ed0724b45a124
eleves <eleves@adm-pret-07.emse2000.local>
2022-01-18 01:49:54 +0100

    Structuring the project

--- app.py
+++ app.py
@@ -1,12 +1,174 @@
-from flask import Flask
-
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_swagger_ui import get_swaggerui_blueprint
 app = Flask(__name__)
+app.secret_key = "Secret Key"
+
+# SqlAlchemy Database Configuration With Mysql
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/immobilier'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+good_identifier = db.Table('good_identifier',
+    db.Column('good_id', db.Integer, db.ForeignKey('goods.good_id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('users.user_id'))
+)
+
+# Creating model table for our CRUD database
+class Good(db.Model):
+    __tablename__ = 'goods'
+    good_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    description= db.Column(db.String(100))
+    type = db.Column(db.String(100))
+    city = db.Column(db.String(100))
+    characteristics = db.Column(db.String(100))
+    user_id=db.Column(db.Integer,db.ForeignKey('users.user_id'))
+    def __init__(self, name, description, type,city,characteristics,user_id):
+        self.name = name
+        self.description= description
+        self.type = type
+        self.city = city
+
+        self. characteristics= characteristics
+        self.user_id = user_id
 
 
+
+class User(db.Model):
+    __tablename__ = 'users'
+    user_id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    birth_date = db.Column(db.String(100))
+    goods = db.relationship("Good",
+                               secondary=good_identifier)
+    isOwner = db.Column(db.Boolean,default=False, nullable=False);
+
+    city = db.Column(db.String(100))
+
+    def __init__(self, first_name, last_name, birth_date,isOwner,city):
+        self.first_name = first_name
+        self.last_name= last_name
+        self.birth_date = birth_date
+        self.isOwner = isOwner
+
+
+
+        self.city = city
+
+
+
+
+# This is the index route where we are going to
+# query on all our Users data
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def Index():
+    all_data = User.query.all()
+
+    return render_template("index.html", employees=all_data)
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/<id>')
+def User_HomePage(id):
+    all_data = User.query.get(id)
+    goods = Good.query.filter(Good.city == "Paris")
+    return render_template("user.html", employees=all_data, Goods=goods)
+
+
+
+
+@app.route('/insert_user', methods=['POST'])
+def insert_user():
+        if request.method == 'POST':
+            first_name = request.form['first_name']
+            last_name = request.form['last_name']
+            birth_date = request.form['birth_date']
+            isOwner=  bool(request.form['isOwner'])
+
+
+            city = request.form['city']
+
+            my_data = User(first_name,last_name, birth_date,isOwner,city)
+            db.session.add(my_data)
+            db.session.commit()
+
+            flash("User Inserted Successfully")
+
+            return request.form
+
+@app.route('/insert_good/<id>', methods=['POST'])
+def insert_good(id):
+        if request.method == 'POST':
+            name = request.form['name']
+            description= request.form['description']
+            type = request.form['type']
+
+
+            city = request.form['city']
+            characteristics = request.form['characteristics']
+            user_id=request.form['user_id']
+            all_data = User.query.get(user_id)
+            my_data = Good(name,description,type,city,characteristics, user_id)
+            db.session.add(my_data)
+            db.session.commit()
+
+            flash("Good Inserted Successfully")
+
+            return redirect(url_for('User_HomePage', id=id))
+
+
+# this is our update route where we are going to update our employee
+@app.route('/update_user/<id>', methods=['POST'])
+def update(id):
+    if request.method == 'POST':
+        my_data = User.query.get(id)
+        my_data.first_name = request.form['first_name']
+        my_data.last_name = request.form['last_name']
+
+        my_data.birth_date = request.form['birth_date']
+        my_data.city = request.form['city']
+
+
+
+        db.session.add(my_data)
+        db.session.commit()
+        flash("Employee Updated Successfully")
+
+        return redirect(url_for('User_HomePage',id=id))
+
+
+
+
+
+
+# This is the index route where we are going to
+# query on all our Goods data
+
+@app.route('/<id>')
+def Good_HomePage(id):
+    all_data = User.query.get(id)
+    return render_template("user.html", employees=all_data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
